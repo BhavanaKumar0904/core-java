40c7fa430f128fff6721e6e2d6970cc8745ea261
BhavanaKumar0904 <bhavanaprarthu@gmail.com>
2024-06-25 20:23:22 +0530

    First commit

--- B96javacore/src/com/tnsif/encapsulation/Encapsulation.java
+++ B96javacore/src/com/tnsif/encapsulation/Encapsulation.java
@@ -2,11 +2,41 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum;//data variable or data member
-	String name;
-	int age;
+	//if no access specifier is mentioned then it will be default
+	private	int serialNum;//data variable or data member
+	private String name;
+	private int age;
 	
+	public int getSerialNum() {
+		return serialNum;
+	}
+
+	public void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	void show(){//member function
 		System.out.println("Serail Number:"+serialNum+"\t Name:"+name+"\t age:"+ age);
+	}
+
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
 	}
 }
--- B96javacore/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ B96javacore/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -5,10 +5,15 @@ public class EncapsulationDemo {
 	public static void main(String[] args)// should not have the logic this is only for interaction
 	{
 		Encapsulation ob = new Encapsulation();//creating an object
-		ob.name="abc";//calling the variable
-		ob.age=33;
-		ob.serialNum=22;
-		ob.show();
+		//ob.name="abc";//calling the variable
+		//ob.age=33;
+		//ob.serialNum=22;
+		
+ob.setSerialNum(22);
+ob.setAge(20);
+ob.setName("abc");
+//ob.show();
+System.out.println(ob);//without tostring() method it will give the location of the object
 
 	}
 
